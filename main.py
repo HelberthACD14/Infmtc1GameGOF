85737f6aa172c7ea0b6b23c24edab67e40545e55
HelberthACD14 <helberth1488@gmail.com>
2021-10-01 12:57:52 -0500

    Un zombie persiguiendo

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def game():
   pygame.init()
   director = Director()
   director.set_constructor(ConstructorHumanos())
+  director2 = Director()
+  director2.set_constructor(ConstructorZombies())
   img_inicio = cargar_imagen(ImagenCarga)
   img_fondo = cargar_imagen('imagenes/fondo.jpg')
   img_banner = cargar_imagen('imagenes/Banner.png')
@@ -18,6 +20,7 @@ def game():
 
   heroe = director.get_heroe()
   banner = director.get_banner()
+  zombie = director2.get_zombie()
   jugando = False
   while True:
     for event in pygame.event.get():
@@ -26,13 +29,18 @@ def game():
     teclas = pygame.key.get_pressed()
     if teclas[K_SPACE]:
       heroe.ubicar((SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
+      zombie.ubicar((0, 0))
       jugando = True
     if jugando:
       screen.blit(img_fondo, (0, 0))
       screen.blit(img_banner, (0, SCREEN_HEIGHT-40))
       heroe.update()
+      print(heroe.rect.x)
+      print(heroe.rect.y)
+      zombie.update(heroe.rect.x,heroe.rect.y)
       banner.update()
       heroe.draw(screen)
+      zombie.draw(screen)
       banner.draw(screen)
     else:
       screen.blit(img_inicio, (0, 0))
---
+++ modelos/zombies.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from copy import copy, deepcopy
+from pygame import sprite
+from pygame.sprite import Sprite
+from modelos.gameConfig import *
+
+class Zombie(Sprite):
+
+    def __init__(self):
+        Sprite.__init__(self)
+        self.sentido = 0
+        self.velocidad = 2
+        self.ataque = 25
+        self.cont = 0
+
+    def ubicar(self, pos):
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+    def set_sprites(self, sprites):
+        self.imagenes = sprites
+        self.image = self.imagenes[self.sentido][0]
+        self.rect = self.image.get_rect()
+
+    def update(self, Xper, Yper):
+        xDelta= Xper - self.rect.x 
+        yDelta= Yper - self.rect.y
+        IrIzquierda=xDelta<-(self.velocidad)
+        IrDerecha=xDelta>(self.velocidad)
+        IrAbajo=yDelta>(self.velocidad)
+        IrArriba=yDelta<-(self.velocidad)
+        if IrIzquierda: 
+            self.rect.left -= self.velocidad
+            self.sentido = 1
+        if IrDerecha:
+            self.rect.left += self.velocidad
+            self.sentido = 0
+        if IrAbajo:
+            self.rect.top += self.velocidad
+            self.sentido = 2
+        if IrArriba:
+            self.rect.top -= self.velocidad
+            self.sentido = 3
+    
+        if IrIzquierda or IrDerecha or IrArriba or IrAbajo:
+            self.image = self.imagenes[self.sentido][self.cont]
+            self.cont += 1
+            self.cont %= 3
+
+            self.rect.top %= SCREEN_HEIGHT-60
+            self.rect.left %= SCREEN_WIDTH
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
