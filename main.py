96975b19c9d958b2f6ae0fde344b85d610dc5c5f
HelberthACD14 <helberth1488@gmail.com>
2021-10-01 13:47:15 -0500

    3 zombies aleatorios prototipados

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import sys
 from modelos.util import cargar_imagen
 from modelos.constructores import *
 from modelos.gameConfig import *
+import random
 
 def game():
   pygame.init()
   director = Director()
   director.set_constructor(ConstructorHumanos())
-  director2 = Director()
-  director2.set_constructor(ConstructorZombies())
+  madScientist = Director()
+  madScientist.set_constructor(ConstructorZombies())
   img_inicio = cargar_imagen(ImagenCarga)
   img_fondo = cargar_imagen('imagenes/fondo.jpg')
   img_banner = cargar_imagen('imagenes/Banner.png')
@@ -20,7 +21,12 @@ def game():
 
   heroe = director.get_heroe()
   banner = director.get_banner()
-  zombie = director2.get_zombie()
+  zombie1 = madScientist.get_zombie()
+  print(zombie1)
+  zombies = []
+  for i in range(3):
+    zombies.append(madScientist.get_zombie())
+  print(zombies)
   jugando = False
   while True:
     for event in pygame.event.get():
@@ -29,18 +35,19 @@ def game():
     teclas = pygame.key.get_pressed()
     if teclas[K_SPACE]:
       heroe.ubicar((SCREEN_WIDTH/2,SCREEN_HEIGHT/2))
-      zombie.ubicar((0, 0))
+      for i in zombies:
+        i.ubicar((random.randint(0, SCREEN_WIDTH),random.randint(0, SCREEN_HEIGHT)))
       jugando = True
     if jugando:
       screen.blit(img_fondo, (0, 0))
       screen.blit(img_banner, (0, SCREEN_HEIGHT-40))
       heroe.update()
-      print(heroe.rect.x)
-      print(heroe.rect.y)
-      zombie.update(heroe.rect.x,heroe.rect.y)
+      for i in zombies:
+        i.update(heroe.rect.x,heroe.rect.y)
       banner.update()
       heroe.draw(screen)
-      zombie.draw(screen)
+      for i in zombies:
+        i.draw(screen)
       banner.draw(screen)
     else:
       screen.blit(img_inicio, (0, 0))
